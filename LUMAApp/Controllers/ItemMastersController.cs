2f3e223e3243818554e5e835ba159ee0ffc07517
Ritick Ranjan <ritickranjan411@gmail.com>
2023-08-16 18:20:28 +0530

    new update

---
+++ LUMAApp/Controllers/ItemMastersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LUMAApp.Entities;
+
+namespace LUMAApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemMastersController : ControllerBase
+    {
+        private readonly Luma1Context _context;
+
+        public ItemMastersController(Luma1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ItemMasters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ItemMaster>>> GetItemMasters()
+        {
+          if (_context.ItemMasters == null)
+          {
+              return NotFound();
+          }
+            return await _context.ItemMasters.ToListAsync();
+        }
+
+        // GET: api/ItemMasters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ItemMaster>> GetItemMaster(string id)
+        {
+          if (_context.ItemMasters == null)
+          {
+              return NotFound();
+          }
+            var itemMaster = await _context.ItemMasters.FindAsync(id);
+
+            if (itemMaster == null)
+            {
+                return NotFound();
+            }
+
+            return itemMaster;
+        }
+
+        // PUT: api/ItemMasters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItemMaster(string id, ItemMaster itemMaster)
+        {
+            if (id != itemMaster.ItemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(itemMaster).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemMasterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ItemMasters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ItemMaster>> PostItemMaster(ItemMaster itemMaster)
+        {
+          if (_context.ItemMasters == null)
+          {
+              return Problem("Entity set 'Luma1Context.ItemMasters'  is null.");
+          }
+            _context.ItemMasters.Add(itemMaster);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ItemMasterExists(itemMaster.ItemId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetItemMaster", new { id = itemMaster.ItemId }, itemMaster);
+        }
+
+        // DELETE: api/ItemMasters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItemMaster(string id)
+        {
+            if (_context.ItemMasters == null)
+            {
+                return NotFound();
+            }
+            var itemMaster = await _context.ItemMasters.FindAsync(id);
+            if (itemMaster == null)
+            {
+                return NotFound();
+            }
+
+            _context.ItemMasters.Remove(itemMaster);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ItemMasterExists(string id)
+        {
+            return (_context.ItemMasters?.Any(e => e.ItemId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ LUMAApp/Controllers/LoanCardMastersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LUMAApp.Entities;
+
+namespace LUMAApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanCardMastersController : ControllerBase
+    {
+        private readonly Luma1Context _context;
+
+        public LoanCardMastersController(Luma1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LoanCardMasters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LoanCardMaster>>> GetLoanCardMasters()
+        {
+          if (_context.LoanCardMasters == null)
+          {
+              return NotFound();
+          }
+            return await _context.LoanCardMasters.ToListAsync();
+        }
+
+        // GET: api/LoanCardMasters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LoanCardMaster>> GetLoanCardMaster(string id)
+        {
+          if (_context.LoanCardMasters == null)
+          {
+              return NotFound();
+          }
+            var loanCardMaster = await _context.LoanCardMasters.FindAsync(id);
+
+            if (loanCardMaster == null)
+            {
+                return NotFound();
+            }
+
+            return loanCardMaster;
+        }
+
+        // PUT: api/LoanCardMasters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLoanCardMaster(string id, LoanCardMaster loanCardMaster)
+        {
+            if (id != loanCardMaster.LoanId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(loanCardMaster).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoanCardMasterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LoanCardMasters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<LoanCardMaster>> PostLoanCardMaster(LoanCardMaster loanCardMaster)
+        {
+          if (_context.LoanCardMasters == null)
+          {
+              return Problem("Entity set 'Luma1Context.LoanCardMasters'  is null.");
+          }
+            _context.LoanCardMasters.Add(loanCardMaster);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (LoanCardMasterExists(loanCardMaster.LoanId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetLoanCardMaster", new { id = loanCardMaster.LoanId }, loanCardMaster);
+        }
+
+        // DELETE: api/LoanCardMasters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLoanCardMaster(string id)
+        {
+            if (_context.LoanCardMasters == null)
+            {
+                return NotFound();
+            }
+            var loanCardMaster = await _context.LoanCardMasters.FindAsync(id);
+            if (loanCardMaster == null)
+            {
+                return NotFound();
+            }
+
+            _context.LoanCardMasters.Remove(loanCardMaster);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LoanCardMasterExists(string id)
+        {
+            return (_context.LoanCardMasters?.Any(e => e.LoanId == id)).GetValueOrDefault();
+        }
+    }
+}
